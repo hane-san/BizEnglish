78e05bd62b09179d92f300a21ae38bf8a0490c2e
hane-san <shun.y.0127@gmail.com>
2026-06-30 18:48:18 +0900

    Add cards loader

---
+++ src/cards.js
@@ -0,0 +1,3 @@
+// Compatibility loader for GitHub Pages uploads from iPhone.
+// index.html expects src/cards.js, while cards.js may live at the repository root.
+document.write('<script src="../cards.js?v=20260630-1"><\/script>');
