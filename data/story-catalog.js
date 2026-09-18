ddcb85624247b92e51f0c79e8f5ff602d6d18a09
hane-san <shun.y.0127@gmail.com>
2026-09-18 18:33:16 +0900

    Publish Kokoro middle section through 027

--- data/story-catalog.js
+++ data/story-catalog.js
@@ -3,5 +3,5 @@ window.STORY_CATALOG=[
   {id:'tono',icon:'⛰️',title:'遠野物語',enTitle:'Tales of Tono',notionParent:'3ca2a8b8-a5c6-819c-b90e-c89d5ee8e397',mode:'sections',base:'data/tono/',sectionIds:['001','002','003','004','005','006','007','008','009','010','011','012','013','014','015']},
   {id:'rashomon',icon:'⛩️',title:'羅生門',enTitle:'Rashomon',notionParent:'3ca2a8b8-a5c6-81fb-a9b1-de78108edf3e',mode:'sections',base:'data/rashomon/',sectionIds:['001','002','003','004','005']},
   {id:'overcoat',icon:'🧥',title:'外套',enTitle:'The Overcoat',notionParent:'3cb2a8b8-a5c6-812e-9a09-cc873f5e930e',mode:'sections',base:'data/overcoat/',sectionIds:['001','002','003','004','005','006','007','008','009','010','011','012','013','014','015']},
-  {id:'kokoro',icon:'🫀',title:'こころ',enTitle:'Kokoro',notionParent:'3df2a8b8-a5c6-8186-8db4-fb2a7d1557e7',mode:'sections',base:'data/kokoro/',sectionIds:['001','002','003','004','005','006','007','008','009','010','011','012','013','014','015','016','017','018','019','020','021']}
+  {id:'kokoro',icon:'🫀',title:'こころ',enTitle:'Kokoro',notionParent:'3df2a8b8-a5c6-8186-8db4-fb2a7d1557e7',mode:'sections',base:'data/kokoro/',sectionIds:['001','002','003','004','005','006','007','008','009','010','011','012','013','014','015','016','017','018','019','020','021','022','023','024','025','026','027']}
 ];
